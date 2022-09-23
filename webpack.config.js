b66e7716ad60b83215c06e532f3abf0c87d2a434
Buddhadeb das <dev48192@gmail.com>
2022-09-23 06:11:50 +0000

    styles

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports ={
           use: "babel-loader",
           exclude: /node_modules/,
         },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              "style-loader",
+              "css-loader",
+              "sass-loader",
+            ],
+          },
       ],
     },
     plugins: [
